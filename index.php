2bb963a90527e1894b5779c7f7f8fce1e49c6f83
Mike Loel Balbon <fdc.balbon@gmail.com>
2022-03-24 14:05:40 +0800

    master: recommit

---
+++ index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sugoi ne!</title>
+</head>
+<body>
+
+    <table border="1" width="600px">
+         <!-- this is for the banner -->
+        <tr>
+            <td colspan="3">
+                <table>
+                    <tr>
+                         <img width="50%" src="./20220226_061132.jpg" alt="">   
+                    </tr>
+                    <tr>
+                         <img width="50%" src="./20220226_061132.jpg" alt="">   
+                    </tr>
+                </table>
+            </td>
+        </tr>
+
+
+        <tr>
+            <td rowspan=1></td>
+            <td>2</td>
+            <td>3</td>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>2</td>
+            <td>3</td>
+        </tr>
+         <!-- this is for the banner -->
+        <tr>
+            <td bgcolor="red">
+                <div>
+                    <h4>Top Places</h4>
+                    <ol>
+                        <li>Mango</li>
+                        <li>Busay</li>
+                        <li>Uptown Vibes</li>
+                        <li>Mangahan</li>
+                        <li>Sagingan</li>
+                    </ol>
+                </div>
+
+
+            </td>
+            <td bgcolor="blue">
+                <div>
+                    <h4>Top Foods</h4>
+                    <ol>
+                        <li>Cow Balbakua</li>
+                        <li>Pig humba</li>
+                        <li>Chicken joy</li>
+                        <li>Sisig</li>
+                        <li>Sardinas</li>
+                    </ol>
+                </div>
+            </td>
+            <td bgcolor="green">
+            <div>
+                    <h4>Top Websites</h4>
+                    <ol>
+                        <li>
+                            <a target="_blank" href="https://www.youtube.com">Youtube</a>
+                        </li>
+                        <li>
+                            <a href="https://www.twitter.com">Twitter</a>
+                        </li>
+                        <li>
+                            <a href="https://www.fb.me">Facebook</a>
+                        </li>
+                        <li>Surf</li>
+                        <li>All girls</li>
+                    </ol>
+                </div>
+            </td>
+        </tr>
+        <!-- this is for the blog -->
+        <tr>
+            <td colspan="3">
+                <ul>
+                    <li>
+                        <h4>
+                            My first love
+                        </h4>
+                        <p>
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto, nihil magnam doloremque hic laborum ducimus doloribus non veniam officia praesentium commodi id, placeat ab voluptatum neque quidem unde fugit cumque?
+                        </p>
+                    </li>
+                    <li>
+                        <h4>
+                            My second love
+                        </h4>
+                        <p>
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto, nihil magnam doloremque hic laborum ducimus doloribus non veniam officia praesentium commodi id, placeat ab voluptatum neque quidem unde fugit cumque?
+                        </p>
+                    </li>
+                </ul>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="3">
+                Copyright 2022
+            </td>
+        </tr>
+       
+
+    </table>
+
+</body>
+</html>
