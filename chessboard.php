a05a914e136423d962ce920bd2103c9310ba6717
Mike Loel Balbon <fdc.balbon@gmail.com>
2022-03-24 13:18:27 +0800

    Chessboard fix

---
+++ chessboard.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My first chessboard</title>
+</head>
+<body>
+<table border="1" width="1000px" height="1000px" bgcolor="#c19a6b">
+    <tr width="100px" height="100px" align="center">
+       <td width="100px" height="100px"></td>
+       <td width="100px" height="100px"><font size="10"><strong>A</strong></font></td>
+       <td width="100px" height="100px"><font size="10"><strong>B</strong></font></td>
+       <td width="100px" height="100px"><font size="10"><strong>C</strong></font></td>
+       <td width="100px" height="100px"><font size="10"><strong>D</strong></font></td>
+       <td width="100px" height="100px"><font size="10"><strong>E</strong></font></td>
+       <td width="100px" height="100px"><font size="10"><strong>F</strong></font></td>
+       <td width="100px" height="100px"><font size="10"><strong>G</strong></font></td>
+       <td width="100px" height="100px"><font size="10"><strong>H</strong></font></td>
+       <td width="100px" height="100px"></td>
+    </tr>
+    <tr width="100px" height="100px" align="center">
+       <td width="100px" height="100px"><font size="10"><strong>8</strong></font></td>
+       <td width="100px" height="100px" bgcolor="white"></td>
+       <td width="100px" height="100px" bgcolor="black"></td>
+       <td width="100px" height="100px" bgcolor="white"></td>
+       <td width="100px" height="100px" bgcolor="black"></td>
+       <td width="100px" height="100px" bgcolor="white"></td>
+       <td width="100px" height="100px" bgcolor="black"></td>
+       <td width="100px" height="100px" bgcolor="white"></td>
+       <td width="100px" height="100px" bgcolor="black"></td>
+       <td width="100px" height="100px"><font size="10"><strong>8</strong></font></td>
+    </tr>
+    <tr width="100px" height="100px" align="center">
+       <td width="100px" height="100px"><font size="10"><strong>7</strong></font></td>
+       <td width="100px" height="100px" bgcolor="black"></td>
+       <td width="100px" height="100px" bgcolor="white"></td>
+       <td width="100px" height="100px" bgcolor="black"></td>
+       <td width="100px" height="100px" bgcolor="white"></td>
+       <td width="100px" height="100px" bgcolor="black"></td>
+       <td width="100px" height="100px" bgcolor="white"></td>
+       <td width="100px" height="100px" bgcolor="black"></td>
+       <td width="100px" height="100px" bgcolor="white"></td>
+       <td width="100px" height="100px"><font size="10"><strong>7</strong></font></td>
+    </tr>
+    <tr width="100px" height="100px" align="center">
+       <td width="100px" height="100px"><font size="10"><strong>6</strong></font></td>
+       <td width="100px" height="100px" bgcolor="white"></td>
+       <td width="100px" height="100px" bgcolor="black"></td>
+       <td width="100px" height="100px" bgcolor="white"></td>
+       <td width="100px" height="100px" bgcolor="black"></td>
+       <td width="100px" height="100px" bgcolor="white"></td>
+       <td width="100px" height="100px" bgcolor="black"></td>
+       <td width="100px" height="100px" bgcolor="white"></td>
+       <td width="100px" height="100px" bgcolor="black"></td>
+       <td width="100px" height="100px"><font size="10"><strong>6</strong></font></td>
+    </tr>
+    <tr width="100px" height="100px" align="center">
+       <td width="100px" height="100px"><font size="10"><strong>5</strong></font></td>
+       <td width="100px" height="100px" bgcolor="black"></td>
+       <td width="100px" height="100px" bgcolor="white"></td>
+       <td width="100px" height="100px" bgcolor="black"></td>
+       <td width="100px" height="100px" bgcolor="white"></td>
+       <td width="100px" height="100px" bgcolor="black"></td>
+       <td width="100px" height="100px" bgcolor="white"></td>
+       <td width="100px" height="100px" bgcolor="black"></td>
+       <td width="100px" height="100px" bgcolor="white"></td>
+       <td width="100px" height="100px"><font size="10"><strong>5</strong></font></td>
+    </tr>
+    <tr width="100px" height="100px" align="center">    
+        <td width="100px" height="100px" align="center"><font size="10"><strong>4</strong></font></td>
+       <td width="100px" height="100px" bgcolor="white"></td>
+       <td width="100px" height="100px" bgcolor="black"></td>
+       <td width="100px" height="100px" bgcolor="white"></td>
+       <td width="100px" height="100px" bgcolor="black"></td>
+       <td width="100px" height="100px" bgcolor="white"></td>
+       <td width="100px" height="100px" bgcolor="black"></td>
+       <td width="100px" height="100px" bgcolor="white"></td>
+       <td width="100px" height="100px" bgcolor="black"></td>
+       <td width="100px" height="100px" align="center"><font size="10"><strong>4</strong></font></td>
+    </tr>
+    <tr width="100px" height="100px" align="center">
+       <td width="100px" height="100px"><font size="10"><strong>3</strong></font></td>
+       <td width="100px" height="100px" bgcolor="black"></td>
+       <td width="100px" height="100px" bgcolor="white"></td>
+       <td width="100px" height="100px" bgcolor="black"></td>
+       <td width="100px" height="100px" bgcolor="white"></td>
+       <td width="100px" height="100px" bgcolor="black"></td>
+       <td width="100px" height="100px" bgcolor="white"></td>
+       <td width="100px" height="100px" bgcolor="black"></td>
+       <td width="100px" height="100px" bgcolor="white"></td>
+       <td width="100px" height="100px"><font size="10"><strong>3</strong></font></td>
+    </tr>
+    <tr width="100px" height="100px" align="center">
+        <td width="100px" height="100px" align="center"><font size="10"><strong>2</strong></font></td>
+       <td width="100px" height="100px" bgcolor="white"></td>
+       <td width="100px" height="100px" bgcolor="black"></td>
+       <td width="100px" height="100px" bgcolor="white"></td>
+       <td width="100px" height="100px" bgcolor="black"></td>
+       <td width="100px" height="100px" bgcolor="white"></td>
+       <td width="100px" height="100px" bgcolor="black"></td>
+       <td width="100px" height="100px" bgcolor="white"></td>
+       <td width="100px" height="100px" bgcolor="black"></td>
+       <td width="100px" height="100px" align="center"><font size="10"><strong>2</strong></font></td>
+    </tr>
+    <tr width="100px" height="100px" align="center">
+       <td width="100px" height="100px"><font size="10"><strong>1</strong></font></td>
+       <td width="100px" height="100px" bgcolor="black"></td>
+       <td width="100px" height="100px" bgcolor="white"></td>
+       <td width="100px" height="100px" bgcolor="black"></td>
+       <td width="100px" height="100px" bgcolor="white"></td>
+       <td width="100px" height="100px" bgcolor="black"></td>
+       <td width="100px" height="100px" bgcolor="white"></td>
+       <td width="100px" height="100px" bgcolor="black"></td>
+       <td width="100px" height="100px" bgcolor="white"></td>
+       <td width="100px" height="100px"><font size="10"><strong>1</strong></font></td>
+    </tr>
+    <tr width="100px" height="100px" align="center">
+       <td width="100px" height="100px"><strong></strong></td>
+       <td width="100px" height="100px"><font size="10"><strong>A</strong></font></td>
+       <td width="100px" height="100px"><font size="10"><strong>B</strong></font></td>
+       <td width="100px" height="100px"><font size="10"><strong>C</strong></font></td>
+       <td width="100px" height="100px"><font size="10"><strong>D</strong></font></td>
+       <td width="100px" height="100px"><font size="10"><strong>E</strong></font></td>
+       <td width="100px" height="100px"><font size="10"><strong>F</strong></font></td>
+       <td width="100px" height="100px"><font size="10"><strong>G</strong></font></td>
+       <td width="100px" height="100px"><font size="10"><strong>H</strong></font></td>
+       <td width="100px" height="100px"></td>
+    </tr>
+
+</table>
+</body>
+</html>
